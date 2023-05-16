6448ce8f66b564ca904c4b0a519ea224d6e80c1e
@joaolucas-prime <jlucas@grupo-artico.com>
2023-05-16 17:57:59 -0300

    :fire: [FIX] fixed components responsible to show the revenue and expense values using api consumption

--- src/components/Information/index.jsx
+++ src/components/Information/index.jsx
@@ -1,17 +1,39 @@
+import { useState, useEffect } from "react";
 import { Container } from "./styles";
 import { DateContainer } from "../DateContainer";
 import { DataContainer } from "../DataContainer";
 import { Button } from "../Button";
+import { api } from "../../services/api.js";
 
 export const Information = () => {
+  const [revenue, setRevenue] = useState("");
+  const [expense, setExpense] = useState("");
+  const [financialStatement, setFinancialStatement] = useState("");
+
+  const showInformation = () => {
+    api.get("http://localhost:8080/get/financeBalance/14").then((response) => {
+      setRevenue(response.data.balance);
+    });
+  };
+
+  useEffect(() => {
+    api.get("http://localhost:8080/get/financeBalance/14").then((response) => {
+      setRevenue(response.data.balance);
+    });
+    api.get("http://localhost:8080/get/financeBalance/13").then((response) => {
+      setExpense(response.data.balance);
+    });
+    // setFinancialStatement(revenue - balance);
+  }, []);
+
   return (
     <Container>
       <DateContainer title={"Data Inicial"} />
       <DateContainer title={"Data Final"} />
-      <Button>Buscar</Button>
-      <DataContainer title={"Receita"} value={"10000"} />
-      <DataContainer title={"Despesas"} value={"112"} />
-      <DataContainer title={"Balanço"} value={"9888"} />
+      <Button onClick={showInformation}>Buscar</Button>
+      <DataContainer title={"Receita"} value={revenue} />
+      <DataContainer title={"Despesas"} value={expense} />
+      <DataContainer title={"Balanço"} value={financialStatement} />
     </Container>
   );
 };
